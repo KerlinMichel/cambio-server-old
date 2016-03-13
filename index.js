43d1e671dcce585bd6f1a16592a04499d0fa4acd
KerlinMichel <michelkerlin@gmail.com>
2016-03-13 10:18:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,19 +13,38 @@ var express = require('express'),
 
 app.use(bodyParser.json());
 
-var request = require('request');
+var request1 = require('request');
 var url ='http://requestb.in/qjjf5gqj'
+
+var jar = request.jar();
+jar.setCookie(request.cookie("foo=bar"), "http://jwgames.me:8080/bin/ee3303f9-2914-4dcc-bc6e-84c824c6e2b8");
+jar.setCookie(request.cookie("bar=baz"), "http://jwgames.me:8080/bin/ee3303f9-2914-4dcc-bc6e-84c824c6e2b8");
+
+var options = { method: 'POST',
+  url: 'http://jwgames.me:8080/bin/ee3303f9-2914-4dcc-bc6e-84c824c6e2b8',
+  qs: { foo: [ 'bar', 'baz' ] },
+  headers: 
+   { 'content-type': 'application/x-www-form-urlencoded',
+     accept: 'application/json' },
+  form: { foo: 'bar', bar: 'baz' },
+  jar: 'JAR' };
 
 app.post('/', function (req, res) {
     var body = req.body;
     //var trackingNumber = body.msg.tracking_number;
     //var slug = body.msg.slug;
     //var token = body.msg.unique_token;
-    request(url, function (error, response, body) {
+    request1(url, function (error, response, body) {
         if (!error) {
             console.log(body);
         }
     });
+    
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+        console.log(body);
+    });
+    
     console.log(req);
     console.log(body);
     console.log(body.text);
@@ -37,6 +56,8 @@ app.post('/', function (req, res) {
         message: 'ok got it!'
     });
 });
+
+
 
 var server = app.listen(port, function () {
 
