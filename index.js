782237d67a2a37266c0f6f35d9d8ca919b84fcab
KerlinMichel <michelkerlin@gmail.com>
2016-03-13 01:31:58 -0500

    Added files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//
+// # LetsTakeSomeWebHooks
+//
+// A simple webhooks server
+//
+var http = require('http');
+var path = require('path');
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+    var body = req.body;
+    var trackingNumber = body.msg.tracking_number;
+    var slug = body.msg.slug;
+    var token = body.msg.unique_token;
+
+    console.log(trackingNumber, slug, token);
+
+    res.json({
+        message: 'ok got it!'
+    });
+});
+
+var server = app.listen(port, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('LETS GET SOME HOOKS BOI at http://%s:%s', host, port)
+
+});
